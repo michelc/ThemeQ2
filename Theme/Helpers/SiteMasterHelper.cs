03969cf211da8d609903ee1324c61518fee41ed3
michel <michelc@gmail.com>
2010-03-09 16:57:47 +0100

    Ajout d'un HtmlHelper pour gérer le menu qui ajoute une classe "selected" à l'élément en cours (créé dans SiteMasterHelper car concerne Site.Master)

---
+++ Theme/Helpers/SiteMasterHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace Theme.Helpers
+{
+    public static class SiteMasterHelper
+    {
+
+        public static string ActionMenu(this HtmlHelper helper, string linkText, string actionName, string controlName)
+        {
+            if (helper.ViewContext.RouteData.Values["action"].ToString() == actionName)
+            {
+                if (helper.ViewContext.RouteData.Values["controller"].ToString() == controlName)
+                {
+                    return helper.ActionLink(linkText, actionName, controlName, new { @class = "selected" });
+                }
+            }
+            return helper.ActionLink(linkText, actionName, controlName);
+        }
+
+    }
+}
